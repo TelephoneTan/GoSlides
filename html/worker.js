01ee3a9aed00801297e8671897a3f6eca0e523ea
telephone <telephone.tan@qq.com>
2023-06-29 05:58:35 +0800

    worker 改用 periodicSync 和 setInterval

--- html/worker.js
+++ html/worker.js
@@ -3,28 +3,45 @@ self.addEventListener('install', () => {
     self.skipWaiting();
 });
 
-function play(urls, i) {
+const urls = [/*INSERT URLS*/]
+
+let i = 0
+
+async function play() {
     if (i === urls.length) {
         i = 0
     }
-    clients.matchAll({type: "window"})
-        .then((clientList) => {
-            for (const client of clientList) {
-                client.navigate(urls[i])
-            }
-            setTimeout(() => {
-                play(urls, ++i)
-            }, 3500);
-        })
-
+    const cls = await clients.matchAll({type: "window"})
+    for (const client of cls) {
+        await client.navigate(urls[i])
+    }
+    i++
 }
+
+const taskNext = "next"
+
+self.addEventListener("periodicsync", (event) => {
+    if (event.tag === taskNext) {
+        event.waitUntil(play());
+    }
+});
+
+const period = 3500
 
 self.addEventListener('activate', (event) => {
     console.log(`ServiceWorker 激活中...`);
-    play([/*INSERT URLS*/], 0)
     event.waitUntil(new Promise(resolve =>
         resolve((async () => {
             await clients.claim()
+            try {
+                await registration.periodicSync.register(taskNext, {
+                    minInterval: period,
+                });
+            } catch (error) {
+                console.error(`定时任务创建失败：${error}`);
+                setInterval(play, period)
+                await play()
+            }
         })())
-    ).then(() => console.log(`ServiceWorker 已激活`)))
+    ).finally(() => console.log(`ServiceWorker 已激活`)))
 });
